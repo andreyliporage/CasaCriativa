89c90db6df1a653de430ad267d2440f45aa2da9f
Andrey Liporage de Matos <andreyliporage@gmail.com>
2020-03-24 16:15:43 -0300

    second class

---
+++ script.js
@@ -0,0 +1,13 @@
+function onOff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+}
+
+function off() {
+    document
+        .querySelector("footer")
+        .classList
+        .toggle("hide")
+}
